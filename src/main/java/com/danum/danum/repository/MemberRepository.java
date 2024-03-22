cbf3df7dfe37c8bd496c37db26bf6e33dab82558
KIMWOOBIN <woodol4540@naver.com>
2024-03-22 21:44:58 +0900

    feat: findByEmail 추가

--- src/main/java/com/danum/danum/repository/MemberRepository.java
+++ src/main/java/com/danum/danum/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import com.danum.danum.domain.MemberDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberDTO, String> {
-
+    public Optional<MemberDTO> findByEmail(String email);
 }
