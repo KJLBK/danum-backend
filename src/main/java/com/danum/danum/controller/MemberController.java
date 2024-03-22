47d56666e61ebc749d0b79480bdcece156ee7890
KIMWOOBIN <woodol4540@naver.com>
2024-03-22 22:09:38 +0900

    feat: 멤버 로회원가입 구현

---
+++ src/main/java/com/danum/danum/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.danum.danum.controller;
+
+
+import com.danum.danum.domain.MemberDTO;
+import com.danum.danum.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class MemberController {
+
+    @Autowired
+    MemberService memberService;
+
+    @PostMapping("/member/join")
+    public ResponseEntity<?> register(@RequestBody MemberDTO member){
+        memberService.join(member);
+        return ResponseEntity.ok(memberService.join(member));
+    }
+
+}
