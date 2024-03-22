c523278ae7204fe4baa3737bf44a1d0afccea5a5
KIMWOOBIN <woodol4540@naver.com>
2024-03-22 20:24:01 +0900

    feat: JPA MemberRepository 연결

--- src/main/java/com/danum/danum/repository/MemberRepository.java
+++ src/main/java/com/danum/danum/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.danum.danum.repository;
 
+import com.danum.danum.domain.MemberDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<MemberDTO, String> {
 
 }
