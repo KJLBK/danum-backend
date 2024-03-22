f7e1100a3088e217ea9d8617a147c4f51e49cff8
KIMWOOBIN <woodol4540@naver.com>
2024-03-22 16:34:00 +0900

    first commit

---
+++ danum/src/main/java/com/danum/danum/DanumApplication.java
@@ -0,0 +1,13 @@
+package com.danum.danum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanumApplication.class, args);
+	}
+
+}
---
+++ danum/src/test/java/com/danum/danum/DanumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danum.danum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
