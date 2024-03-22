f93144e4f4d0c93020837acae9e1d159e291854d
KIMWOOBIN <woodol4540@naver.com>
2024-03-22 20:23:17 +0900

    feat: 멤버 테이블 제작

--- src/main/java/com/danum/danum/domain/MemberDTO.java
+++ src/main/java/com/danum/danum/domain/MemberDTO.java
@@ -1,11 +1,21 @@
 package com.danum.danum.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 @AllArgsConstructor
+@ToString
+@Table(name = "members")
 public class MemberDTO {
+    @Id
+    @Column(name = "member_email")
+    String email;
+    @Column(name = "member_password")
+    String password;
+    @Column(name = "member_phone")
+    String phone;
 }
