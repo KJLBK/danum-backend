7bc789875e10ecbb77343f250501203e3b4b83ff
KIMWOOBIN <woodol4540@naver.com>
2024-03-23 19:52:12 +0900

    feat: 멤버 에러 코드 추가

--- src/main/java/com/danum/danum/exception/ErrorCode.java
+++ src/main/java/com/danum/danum/exception/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
     PASSWORD_SHORT_EXCEPTION(HttpStatus.BAD_REQUEST, "M001", "비밀번호가 8자보다 적습니다."),
     PASSWORD_LONG_EXCEPTION(HttpStatus.BAD_REQUEST, "M002", "비밀번호가 16자를 초과합니다."),
     DUPLICATION_EXEPTION(HttpStatus.NOT_FOUND, "M003", "이미 중복되는 회원입니다."),
-    LEAVE_EXEPTION(HttpStatus.BAD_REQUEST, "M004", "회원 탈퇴에 실패하셨습니다.");
+    LEAVE_EXEPTION(HttpStatus.BAD_REQUEST, "M004", "회원 탈퇴에 실패하셨습니다."),
+    AWITHOUT_MEMBER(HttpStatus.NOT_FOUND, "M005", "존재하지 않는 사용자입니다."),
+    MISTAKE_PASSWORD(HttpStatus.BAD_REQUEST, "M006", "잘못 된 비밀번호 입력입니다.");
 
     private final HttpStatus status;
 
