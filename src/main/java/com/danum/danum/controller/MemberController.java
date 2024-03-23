8eaba6213a17ccb8b24418b9d82805ede251d630
KIMWOOBIN <woodol4540@naver.com>
2024-03-23 16:45:38 +0900

    feat: ë로그인 기능 추가

--- src/main/java/com/danum/danum/controller/MemberController.java
+++ src/main/java/com/danum/danum/controller/MemberController.java
@@ -2,18 +2,13 @@ package com.danum.danum.controller;
 
 
 import com.danum.danum.domain.MemberDTO;
-import com.danum.danum.exception.ErrorCode;
 import com.danum.danum.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.Optional;
 
 @Controller
 public class MemberController {
@@ -23,15 +18,17 @@ public class MemberController {
 
     @PostMapping("/member/join")
     public ResponseEntity<?> register(@RequestBody MemberDTO member){
-        memberService.join(member);
-        return ResponseEntity.ok("회원 가입에 성공하였습니다.");
+        return ResponseEntity.ok(memberService.join(member));
     }
 
     @DeleteMapping("/member/delete")
     public ResponseEntity<?> delete(@RequestBody MemberDTO member){
-        memberService.delete(member);
-        return ResponseEntity.ok("회원 탈퇴에 성공하였습니다.");
+        return ResponseEntity.ok(memberService.delete(member));
     }
 
+    @PostMapping("/member/login")
+    public ResponseEntity<?> login(@RequestBody MemberDTO member){
+        return ResponseEntity.ok(memberService.login(member));
+    }
 
 }
