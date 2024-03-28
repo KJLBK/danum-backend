f986c6d866af79a52b41bff3d787dac60cc73d53
KIMWOOBIN <woodol4540@naver.com>
2024-03-28 23:02:08 +0900

    fix: 에러 코드 추가

--- src/main/java/com/danum/danum/exception/ErrorCode.java
+++ src/main/java/com/danum/danum/exception/ErrorCode.java
@@ -8,7 +8,8 @@ public enum ErrorCode {
     
     PASSWORD_SHORT_EXCEPTION(HttpStatus.BAD_REQUEST, "M001", "비밀번호가 8자보다 적습니다."),
     PASSWORD_LONG_EXCEPTION(HttpStatus.BAD_REQUEST, "M002", "비밀번호가 16자를 초과합니다."),
-    DUPLICATION_EXEPTION(HttpStatus.BAD_REQUEST, "M003", "이미 사용되는 아이디입니다.");
+    DUPLICATION_EXCEPTION(HttpStatus.BAD_REQUEST, "M003", "이미 사용되는 아이디입니다."),
+    NICKNAME_EXCEPTION(HttpStatus.BAD_REQUEST, "M004", "이미 사용중인 닉네임입니다.");
 
     private final HttpStatus status;
 
@@ -21,4 +22,5 @@ public enum ErrorCode {
         this.code = code;
         this.message = message;
     }
+
 }
