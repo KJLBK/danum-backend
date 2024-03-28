eccb46f5dddea0f730f4311e30b1a40ca5cd162d
KIMWOOBIN <woodol4540@naver.com>
2024-03-28 16:53:53 +0900

    feat: 디스코드 연동 추가

---
+++ src/main/java/com/danum/danum/service/discord/DiscordService.java
@@ -0,0 +1,9 @@
+package com.danum.danum.service.discord;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+interface DiscordService {
+
+}
