8e028c0196c13d527c00a860a6db4d88ae3f5ff3
rdyjun <rdyjun00@gmail.com>
2024-03-30 00:17:11 +0900

    fix: findByEmail 제거 및 findByName 추가

--- src/main/java/com/danum/danum/repository/MemberRepository.java
+++ src/main/java/com/danum/danum/repository/MemberRepository.java
@@ -2,8 +2,16 @@ package com.danum.danum.repository;
 
 import com.danum.danum.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+	@Query("SELECT m FROM Member m WHERE m.name = :name")
+	Optional<Member> findByName(@Param("name") String name);
+
 }
