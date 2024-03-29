b1307179d8cd513cb237e371fdb65e3351d87bb0
rdyjun <rdyjun00@gmail.com>
2024-03-30 00:02:05 +0900

    fix: MemberRepository 객체 변경
    MemberDto 에서 Member로 변경

--- src/main/java/com/danum/danum/repository/MemberRepository.java
+++ src/main/java/com/danum/danum/repository/MemberRepository.java
@@ -1,12 +1,9 @@
 package com.danum.danum.repository;
 
-import com.danum.danum.domain.MemberDTO;
+import com.danum.danum.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface MemberRepository extends JpaRepository<MemberDTO, String> {
-    public Optional<MemberDTO> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<Member, String> {
 }
