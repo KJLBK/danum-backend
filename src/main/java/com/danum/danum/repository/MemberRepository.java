d8a6fc996f56104a95de4014ab1833dcca5a806d
KIMWOOBIN <woodol4540@naver.com>
2024-03-30 14:08:12 +0900

    리포지ífeat :  리 메서드 추가

--- src/main/java/com/danum/danum/repository/MemberRepository.java
+++ src/main/java/com/danum/danum/repository/MemberRepository.java
@@ -1,12 +1,15 @@
 package com.danum.danum.repository;
 
-import com.danum.danum.domain.MemberDTO;
+import com.danum.danum.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<MemberDTO, String> {
-    public Optional<MemberDTO> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<Member, String> {
+    public Optional<Member> findByEmail(String email);
+
+    public Optional<Member> findByName(String name);
+
 }
