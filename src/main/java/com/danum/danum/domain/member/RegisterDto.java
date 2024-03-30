91852c9f9d5accedbc240145562102e5babf988c
KIMWOOBIN <woodol4540@naver.com>
2024-03-30 14:06:30 +0900

    feat: 회원가입 DTO 추가

--- src/main/java/com/danum/danum/domain/member/RegisterDto.java
+++ src/main/java/com/danum/danum/domain/member/RegisterDto.java
@@ -1,10 +1,12 @@
 package com.danum.danum.domain.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RegisterDto {
 
     private String email;
@@ -14,5 +16,9 @@ public class RegisterDto {
     private String phone;
 
     private String name;
+
+    public void settingPassword(String password){
+        this.password = password;
+    }
 
 }
