0f520d4e036b2d67c2f73af26b97db41736b18fc
KIMWOOBIN <woodol4540@naver.com>
2024-03-30 14:07:33 +0900

    feat : 에러 메시지 추가

--- src/main/java/com/danum/danum/exception/ErrorCode.java
+++ src/main/java/com/danum/danum/exception/ErrorCode.java
@@ -9,7 +9,9 @@ public enum ErrorCode {
     PASSWORD_SHORT_EXCEPTION(HttpStatus.BAD_REQUEST, "M001", "비밀번호가 8자보다 적습니다."),
     PASSWORD_LONG_EXCEPTION(HttpStatus.BAD_REQUEST, "M002", "비밀번호가 16자를 초과합니다."),
     DUPLICATION_EXCEPTION(HttpStatus.BAD_REQUEST, "M003", "이미 사용되는 아이디입니다."),
-    NICKNAME_EXCEPTION(HttpStatus.BAD_REQUEST, "M004", "이미 사용중인 닉네임입니다.");
+    NICKNAME_EXCEPTION(HttpStatus.BAD_REQUEST, "M004", "이미 사용중인 닉네임입니다."),
+    PASSWORD_NOTMATCH(HttpStatus.BAD_REQUEST, "M005", "패스워드가 틀렸습니다."),
+    NULLID_EXCEPTION(HttpStatus.BAD_REQUEST, "M006", "존재하지 않는 아이디 입니다.");
 
     private final HttpStatus status;
 
