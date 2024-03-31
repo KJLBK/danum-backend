59d59e4606a2fb2725c5767a227e0049bb1eb6f9
KIMWOOBIN <woodol4540@naver.com>
2024-04-01 00:58:14 +0900

    fix: 메서드 추가

--- src/main/java/com/danum/danum/controller/QuestionController.java
+++ src/main/java/com/danum/danum/controller/QuestionController.java
@@ -1,13 +1,24 @@
 package com.danum.danum.controller;
 
+import com.danum.danum.domain.board.Question;
+import com.danum.danum.domain.board.QuestionNewDto;
 import com.danum.danum.service.board.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
 
     private final QuestionService questionService;
+
+    @PostMapping("/board/NewQuestion")
+    public ResponseEntity<?> created(@RequestBody QuestionNewDto questionNewDto){
+        Question question = questionService.created(questionNewDto);
+        return ResponseEntity.ok(question);
+    }
 
 }
