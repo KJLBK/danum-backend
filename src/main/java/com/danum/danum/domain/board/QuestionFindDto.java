6d34b4371bee38a40522a08c1f61717672696fea
KIMWOOBIN <woodol4540@naver.com>
2024-04-01 00:39:43 +0900

    feat : 아이디 찾기 DTO

---
+++ src/main/java/com/danum/danum/domain/board/QuestionFindDto.java
@@ -0,0 +1,14 @@
+package com.danum.danum.domain.board;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionFindDto {
+
+    private int id;
+
+}
