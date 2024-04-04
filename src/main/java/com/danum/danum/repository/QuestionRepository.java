c38a431ab8f75de8b045a09ccbe9902b9da5bc20
KIMWOOBIN <woodol4540@naver.com>
2024-04-04 20:08:18 +0900

    feat : 데이터 조회 JPA  추가

--- src/main/java/com/danum/danum/repository/QuestionRepository.java
+++ src/main/java/com/danum/danum/repository/QuestionRepository.java
@@ -1,11 +1,16 @@
 package com.danum.danum.repository;
 
 import com.danum.danum.domain.board.Question;
+import com.danum.danum.domain.board.QuestionList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, String> {
+
+    List<QuestionList> findAllBy();
 
 }
