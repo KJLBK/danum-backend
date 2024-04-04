02d7e8688d839558c58e16e0194c99baeba975ff
KIMWOOBIN <woodol4540@naver.com>
2024-04-04 20:06:35 +0900

    feat: 게시판 조회 추가

--- src/main/java/com/danum/danum/controller/QuestionController.java
+++ src/main/java/com/danum/danum/controller/QuestionController.java
@@ -6,8 +6,11 @@ import com.danum.danum.service.board.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,10 +18,15 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
-    @PostMapping("/board/NewQuestion")
+    @PostMapping("/board/newQuestion")
     public ResponseEntity<?> created(@RequestBody QuestionNewDto questionNewDto){
         Question question = questionService.created(questionNewDto);
         return ResponseEntity.ok(question);
+    }
+
+    @GetMapping("/board/serachQuestion")
+    public ResponseEntity<?> serach(){
+        return ResponseEntity.ok(questionService.seauch());
     }
 
 }
