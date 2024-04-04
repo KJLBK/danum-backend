9ae3171f25f6a2c9c6350b25771adf2d0dd0f4b0
KIMWOOBIN <woodol4540@naver.com>
2024-04-04 20:07:43 +0900

    feat: 게시판조회 DTO 추가

---
+++ src/main/java/com/danum/danum/domain/board/QuestionList.java
@@ -0,0 +1,13 @@
+package com.danum.danum.domain.board;
+
+public class QuestionList {
+
+    private int id;
+
+    private String email;
+
+    private String title;
+
+    private String content;
+
+}
