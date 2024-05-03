a3b7dde03c85ecec4422b2321e85475a3030654d
KIMWOOBIN <woodol4540@naver.com>
2024-05-04 06:19:10 +0900

    fix: 게시판 생성 및 게시판 조회 수정

--- src/main/java/com/danum/danum/controller/QuestionController.java
+++ src/main/java/com/danum/danum/controller/QuestionController.java
@@ -1,20 +1,14 @@
 package com.danum.danum.controller;
 
-import com.danum.danum.domain.board.Question;
 import com.danum.danum.domain.board.QuestionNewDto;
 import com.danum.danum.service.board.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.query.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class QuestionController {
 
@@ -22,11 +16,13 @@ public class QuestionController {
 
     @PostMapping("/board/newQuestion")
     public ResponseEntity<?> created(@RequestBody QuestionNewDto questionNewDto){
-        return ResponseEntity.ok(questionService.created(questionNewDto));
+        questionService.created(questionNewDto);
+
+        return ResponseEntity.ok("게시판 생성 성공");
     }
 
-    @GetMapping("/board/serachQuestion")
-    public ResponseEntity<?> search(@RequestParam(value = "page", defaultValue = "0") int page){
+    @GetMapping("/board/searchQuestion/{id}")
+    public ResponseEntity<?> search(@PathVariable("id") int page){
         return ResponseEntity.ok(questionService.search(page));
     }
 
