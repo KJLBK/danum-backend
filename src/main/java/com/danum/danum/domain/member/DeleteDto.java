07863ff86bdc17082c775166bd727cd4c8ad52c1
KIMWOOBIN <woodol4540@naver.com>
2024-05-04 07:32:01 +0900

    feat: 멤버 삭제 dto

---
+++ src/main/java/com/danum/danum/domain/member/DeleteDto.java
@@ -0,0 +1,15 @@
+package com.danum.danum.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteDto {
+
+    private String email;
+
+}
+
