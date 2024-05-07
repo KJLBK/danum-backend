56413f589c1df74466a565fe118b926e1c3a14da
KIMWOOBIN <woodol4540@naver.com>
2024-05-07 21:34:19 +0900

    fix: 어노테이션 분리

--- src/main/java/com/danum/danum/controller/MemberController.java
+++ src/main/java/com/danum/danum/controller/MemberController.java
@@ -1,11 +1,19 @@
 package com.danum.danum.controller;
 
 
-import com.danum.danum.domain.member.*;
+import com.danum.danum.domain.member.DeleteDto;
+import com.danum.danum.domain.member.LoginDto;
+import com.danum.danum.domain.member.Member;
+import com.danum.danum.domain.member.RegisterDto;
+import com.danum.danum.domain.member.UpdateDto;
 import com.danum.danum.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
