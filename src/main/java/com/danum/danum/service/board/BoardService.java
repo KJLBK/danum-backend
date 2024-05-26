f756a9b9de5a1653743cedd14786ce0d6acc2f3a
KROOKIMWOOBIN <woodol4540@naver.com>
2024-05-26 22:04:30 +0900

    fix: 구현체뿐만 아니라 서비스에도 어노테이션을 붙쳐봄

--- src/main/java/com/danum/danum/service/board/BoardService.java
+++ src/main/java/com/danum/danum/service/board/BoardService.java
@@ -3,9 +3,11 @@ package com.danum.danum.service.board;
 import com.danum.danum.domain.board.Board;
 import com.danum.danum.domain.board.BoardUpdateDto;
 import com.danum.danum.domain.board.BoardNewDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface BoardService {
 
     void created(BoardNewDto boardNewDtoDto);
