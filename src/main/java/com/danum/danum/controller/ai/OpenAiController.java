8f7fdf8aacbd899c683136deb397d612f5c8238c
rdyjun <rdyjun00@gmail.com>
2024-06-10 16:23:08 +0900

    feat: open ai 채팅 api 작성

--- src/main/java/com/danum/danum/controller/ai/OpenAiController.java
+++ src/main/java/com/danum/danum/controller/ai/OpenAiController.java
@@ -1,30 +1,36 @@
 package com.danum.danum.controller.ai;
 
-import com.danum.danum.domain.openai.OpenAiRequest;
-import com.danum.danum.domain.openai.OpenAiResult;
+import com.danum.danum.domain.openai.OpenAiUserMessageDto;
 import com.danum.danum.service.ai.OpenAiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/open-ai")
 public class OpenAiController {
 
 	private final OpenAiService openAiService;
 
-//	@GetMapping("/ai-test")
-//	public Map aiTest() {
-//		Map result = aiService.sendMessage("ai의 역사에 대해 50자 내로 설명해줘");
-//		System.out.println(result);
-//
-//		return result;
-//	}
+	@PostMapping("")
+	public ResponseEntity<ChatResponse> generate(@RequestBody OpenAiUserMessageDto message) {
+		return ResponseEntity.ok()
+				.body(openAiService.sendMessage(message));
+	}
 
-	@PostMapping("/open-ai/generate")
-	public OpenAiResult generate(@RequestBody OpenAiRequest question) {
-		return new OpenAiResult(question.getAnswer());
+	@PatchMapping("/{id}")
+	public ResponseEntity<Void> close(@PathVariable("id") Long id) {
+		openAiService.conversationClosed(id);
+
+		return ResponseEntity.ok()
+				.build();
 	}
 
 }
