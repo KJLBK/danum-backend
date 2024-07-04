7cd56f9caaf1cd6490ab68a693aae67949bb31e0
KROOKIMWOOBIN <woodol4540@naver.com>
2024-07-04 22:16:10 +0900

    feat: repository annotation

--- src/main/java/com/danum/danum/repository/VillageCommentRepository.java
+++ src/main/java/com/danum/danum/repository/VillageCommentRepository.java
@@ -2,9 +2,11 @@ package com.danum.danum.repository;
 
 import com.danum.danum.domain.comment.village.VillageComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VillageCommentRepository extends JpaRepository<VillageComment, Long> {
 
     List<VillageComment> findAllByVillageId(Long villageId);
--- src/main/java/com/danum/danum/repository/VillageEmailRepository.java
+++ src/main/java/com/danum/danum/repository/VillageEmailRepository.java
@@ -2,6 +2,8 @@ package com.danum.danum.repository;
 
 import com.danum.danum.domain.board.village.VillageEmailToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VillageEmailRepository extends CrudRepository<VillageEmailToken, Long> {
 }
