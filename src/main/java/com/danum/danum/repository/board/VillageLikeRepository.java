2292989226138a0b6f9d653d9f44742b976365eb
KROOKIMWOOBIN <woodol4540@naver.com>
2024-07-10 13:02:41 +0900

    FEAT: 마을 게시판 좋아요 기능 추가

---
+++ src/main/java/com/danum/danum/repository/board/VillageLikeRepository.java
@@ -0,0 +1,18 @@
+package com.danum.danum.repository.board;
+
+import com.danum.danum.domain.board.question.Question;
+import com.danum.danum.domain.board.question.QuestionLike;
+import com.danum.danum.domain.board.village.Village;
+import com.danum.danum.domain.board.village.VillageLike;
+import com.danum.danum.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VillageLikeRepository extends JpaRepository<VillageLike, Long> {
+
+    Optional<VillageLike> findByVillageIdAndMemberEmail(Village village, Member member);
+
+}
