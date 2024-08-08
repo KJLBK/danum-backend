961e8256aeeee46bbacdc680a1b19c0a12d164a7
alpin87 <qortmdals94@naver.com>
2024-08-08 16:37:35 +0900

    add : 이전 ìRedis ±팅 내역 유지

--- src/main/java/com/danum/danum/controller/chat/ChatController.java
+++ src/main/java/com/danum/danum/controller/chat/ChatController.java
@@ -9,6 +9,8 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 @Controller
 public class ChatController {
 
@@ -18,13 +20,26 @@ public class ChatController {
     @Autowired
     private ChatRoomRepository chatRoomRepository;
 
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
     @MessageMapping("/chat/message")
     public void message(@Payload ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
             chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+
+            // 채팅방에 입장할 때 기존 메시지들을 WebSocket으로 전송
+            List<Object> previousMessages = chatRoomRepository.getMessages(message.getRoomId());
+            for (Object previousMessage : previousMessages) {
+                messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), previousMessage);
+            }
+        } else if (ChatMessage.MessageType.TALK.equals(message.getType())) {
+            // 채팅 메시지를 저장
+            chatRoomRepository.saveChatMessage(message);
         }
-        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+
+        // Websocket에 발행된 메시지를 Redis로 발행한다(publish)
         redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
--- src/main/java/com/danum/danum/service/chat/RedisPublisher.java
+++ src/main/java/com/danum/danum/service/chat/RedisPublisher.java
@@ -1,6 +1,7 @@
 package com.danum.danum.service.chat;
 
 import com.danum.danum.domain.chat.ChatMessage;
+import com.danum.danum.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,8 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisPublisher {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ChatRoomRepository chatRoomRepository;
 
+    // Redis Topic에서 메시지 발생. 메시지 발행 후, 대기 중이던 Redis 구독서비스
     public void publish(ChannelTopic topic, ChatMessage message) {
+        //Redis에 메시지 저장
+        chatRoomRepository.saveChatMessage(message.getRoomId(), message);
+
         log.info("Publishing message: " + message);
         redisTemplate.convertAndSend(topic.getTopic(), message);
     }
