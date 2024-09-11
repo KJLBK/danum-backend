c54a1b732f2e8f52ac73a4518a5f7c009f6fe5ce
alpin87 <qortmdals94@naver.com>
2024-09-11 20:02:48 +0900

    fix: Store cookie after reissuing access token using refresh token

--- src/main/java/com/danum/danum/controller/jwt/AuthController.java
+++ src/main/java/com/danum/danum/controller/jwt/AuthController.java
@@ -2,8 +2,6 @@ package com.danum.danum.controller.jwt;
 
 import com.danum.danum.domain.jwt.TokenBox;
 import com.danum.danum.domain.jwt.TokenDto;
-import com.danum.danum.exception.CustomException;
-import com.danum.danum.exception.ErrorResponse;
 import com.danum.danum.util.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,16 +19,27 @@ public class AuthController {
 
     private final JwtUtil jwtUtil;
 
+    private final static String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+    private final static String ACCESS_TOKEN_COOKIE_NAME = "accessToken";
+
     @PostMapping("/refresh")
-    public ResponseEntity<?> refreshToken(@RequestBody TokenDto refreshTokenDto) {
-        try {
-            TokenBox newTokenBox = jwtUtil.refreshAccessToken(refreshTokenDto.getToken());
-            return ResponseEntity.ok(newTokenBox);
-        } catch (CustomException e) {
-            ErrorResponse errorResponse = new ErrorResponse(e.getHttpStatus().value(), e.getMessage());
-            return ResponseEntity
-                    .status(e.getHttpStatus())
-                    .body(errorResponse);
-        }
+    public ResponseEntity<String> refreshToken(@RequestBody TokenDto refreshTokenDto, HttpServletResponse response) {
+        TokenBox newTokenBox = jwtUtil.refreshAccessToken(refreshTokenDto.getToken());
+
+        // Refresh Token을 쿠키에 설정
+        setCookie(response, REFRESH_TOKEN_COOKIE_NAME, newTokenBox.getRefreshToken().getToken());
+
+        // Access Token을 쿠키에 설정
+        setCookie(response, ACCESS_TOKEN_COOKIE_NAME, newTokenBox.getAccessToken().getToken());
+
+        return ResponseEntity.ok("토큰 재발급 성공");
+    }
+
+    private void setCookie(HttpServletResponse response, String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 }
