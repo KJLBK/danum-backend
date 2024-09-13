7eb647925cc1cd0f00f13bde2e41a84cffacc714
KROOKIMWOOBIN <woodol4540@naver.com>
2024-09-13 23:15:19 +0900

    FEAT: ADMIN SERVIE

--- src/main/java/com/danum/danum/service/admin/AdminService.java
+++ src/main/java/com/danum/danum/service/admin/AdminService.java
@@ -2,16 +2,24 @@ package com.danum.danum.service.admin;
 
 import com.danum.danum.domain.board.question.Question;
 import com.danum.danum.domain.board.village.Village;
+import com.danum.danum.domain.comment.question.QuestionComment;
+import com.danum.danum.domain.comment.village.VillageComment;
 import com.danum.danum.domain.member.Member;
 
 import java.util.List;
 
 public interface AdminService {
 
-    List<Member> viewMembers();
+    List<Member> getMembers();
 
-    List<Question> viewQuestionBoards();
+    List<Question> findBoardsByMemberName();
 
-    List<Village> viewVillageBoards();
+    List<QuestionComment> findCommentByQuestionId();
+
+    List<VillageComment> findCommentByVillageId();
+
+    void updateAllData();
+
+    void updateMemberStatus();
 
 }
--- src/main/java/com/danum/danum/service/admin/AdminServiceImpl.java
+++ src/main/java/com/danum/danum/service/admin/AdminServiceImpl.java
@@ -2,6 +2,8 @@ package com.danum.danum.service.admin;
 
 import com.danum.danum.domain.board.question.Question;
 import com.danum.danum.domain.board.village.Village;
+import com.danum.danum.domain.comment.question.QuestionComment;
+import com.danum.danum.domain.comment.village.VillageComment;
 import com.danum.danum.domain.member.Member;
 import org.springframework.stereotype.Service;
 
@@ -11,18 +13,33 @@ import java.util.List;
 public class AdminServiceImpl implements AdminService {
 
     @Override
-    public List<Member> viewMembers() {
+    public List<Member> getMembers() {
         return null;
     }
 
     @Override
-    public List<Question> viewQuestionBoards() {
+    public List<Question> findBoardsByMemberName() {
         return null;
     }
 
     @Override
-    public List<Village> viewVillageBoards() {
+    public List<QuestionComment> findCommentByQuestionId() {
         return null;
+    }
+
+    @Override
+    public List<VillageComment> findCommentByVillageId() {
+        return null;
+    }
+
+    @Override
+    public void updateAllData() {
+
+    }
+
+    @Override
+    public void updateMemberStatus() {
+
     }
 
 }
